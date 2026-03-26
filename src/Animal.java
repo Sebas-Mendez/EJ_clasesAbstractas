286dea39d674b54dad369951c10b38171b0d1ca5
Sebas-Mendez <sebastianmendezochoa92@gmail.com>
2026-03-26 17:26:42 -0500

    Clase padre + hijas

--- src/Animal.java
+++ src/Animal.java
@@ -1,5 +1,15 @@
-public class Animal {
+abstract class Animal {
+    protected String nombre;
 
+    public Animal(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public abstract void hacerSonido();
+
+    public void dormir() {
+        System.out.println(nombre + " está durmiendo...");
+    }
 
 
 
